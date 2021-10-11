a90b2e144ac67a73c0b553b1d2025f228419c4c5
alittlekitten <dlscjs8646@gmail.com>
2021-10-12 02:29:48 +0900

    :art: 1편 마무리 : list 내용이 바뀔 때마다 console.log 찍기
    1편 마무리

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState} from 'react';
+import React, { Component, useState, useEffect } from 'react'; // 라이프사이클 이후에 추가할 수 있는 것이 useEffect
 import './App.css';
 import List from './List.jsx';
 
@@ -6,14 +6,18 @@ const App = () => {
   const [todos, setTodos] = useState(['js공부']); // 앞은 상태, 뒤는 메소드 반환 https://ko.reactjs.org/docs/hooks-state.html 참조, todos가 바뀌면 자동으로 다시 렌더링
   const [newTodo, setNewTodo] = useState();
 
-const changeInputData = (e) =>{
-    setNewTodo(e.target.value); // 새로운 정보를 newTodo에 넣게 되는 것!
-}
-  
-const addTodo = (e) => {
-  e.preventDefault();
-  setTodos([...todos, newTodo]);
-}
+  const changeInputData = (e) =>{
+      setNewTodo(e.target.value); // 새로운 정보를 newTodo에 넣게 되는 것!
+  }
+    
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, newTodo]);
+  }
+
+  useEffect( () => {
+    console.log("새로운 내용이 렌더링됐네요", todos);
+  }, [todos]) // 새로운 내용이 추가가 됐다는 것을 콜백함수로 추가시켜주고싶으면 todos의 변경을 지켜보면 된다!
 
   return (
     <>
